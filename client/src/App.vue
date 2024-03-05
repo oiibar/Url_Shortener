ec8c24abfb4a1516bf6b9ab9a95af235bde7286e
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-03-05 20:24:03 +0600

    fixed styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,42 +1,53 @@
 <!-- App.vue -->
 <template>
-  <div className="container">
-    <h1>URL Shortener</h1>
-    <input
-      v-model="longUrl"
-      placeholder="Enter your long URL"
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="url"
-      type="text"
-    />
-    <button
-      @click="shortenUrl"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-      type="button"
-    >
-      Shorten
-    </button>
-    <p v-if="shortenedUrl">
-      <a :href="getFullUrl(shortenedUrl)" target="_blank">
-        http://localhost:5000/{{ shortenedUrl }}
-      </a>
-    </p>
-    <p v-if="error" style="color: red">Error: {{ error }}</p>
-    <div>
-      <h2>Saved URLs</h2>
-      <ul>
-        <li v-for="url in storedUrls" :key="url.shortUrl">
-          <a :href="getFullUrl(url.shortUrl)" target="_blank">
-            http://localhost:5000/{{ url.shortUrl }}
+  <body>
+    <div class="flex justify-center items-center text-center flex-col p-10">
+      <h1 class="text-3xl font-medium mb-32">URL Shortener</h1>
+      <form class="mb-32" action="#">
+        <input
+          v-model="longUrl"
+          class="mr-5 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter your long URL"
+          id="url"
+          type="text"
+        />
+        <button
+          @click="shortenUrl"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Shorten
+        </button>
+      </form>
+      <div class="flex text-center items-center justify-center">
+        <p class="text-xl mr-5" v-if="shortenedUrl">
+          <a :href="getFullUrl(shortenedUrl)" target="_blank">
+            http://localhost:5000/{{ shortenedUrl }}
           </a>
-        </li>
-      </ul>
+        </p>
+      </div>
+      <p v-if="error" style="color: red">Error: {{ error }}</p>
+      <div class="flex text-center items-center justify-center">
+        <ul class="text-xl mr-5">
+          <li v-for="url in storedUrls" :key="url.shortUrl">
+            <a :href="getFullUrl(url.shortUrl)" target="_blank">
+              http://localhost:5000/{{ url.shortUrl }}
+            </a>
+          </li>
+        </ul>
+        <button
+          @click="clearStoredUrls"
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Clear All
+        </button>
+      </div>
     </div>
-  </div>
+  </body>
 </template>
 
 <script>
-import "./global.css";
+import "./assests/css/tailwind.css";
 export default {
   data() {
     return {
@@ -87,12 +98,20 @@ export default {
     getFullUrl(shortenedUrl) {
       return `http://localhost:5000/${shortenedUrl}`;
     },
+    clearStoredUrls() {
+      // Clear all stored URLs in localStorage
+      localStorage.removeItem("storedUrls");
+      // Clear the displayed URLs in the component
+      this.shortenedUrl = "";
+      this.error = null;
+    },
   },
 };
 </script>
 
 <style>
-.container {
+body {
   font-family: "Montserrat";
+  background-color: #fbeec1;
 }
 </style>
